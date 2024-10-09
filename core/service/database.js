cb989b2b2d244596930ac836a5773608bf43004a
Giulia Floris <>
2024-10-09 17:39:38 +0200

    [ADD] DB infos

--- core/service/database.js
+++ core/service/database.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 // Create a connection pool
 const pool = mysql.createPool({
-    host: "",
-    user: "",
-    password: "",
-    database: ""
+    host: "127.0.0.1",
+    user: "root",
+    password: "uniSignMiUp2024",
+    database: "UniSignMiUp"
 });
 
 // Export the pool for use in other files
