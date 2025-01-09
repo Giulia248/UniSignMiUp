2d26589e0bb8a6cc55882311624cceef7263fb84
Giulia Floris <giulia.floris@accenture.com>
2025-01-09 12:51:53 +0100

    [ADD] mock to dashboard

--- features/Dashboard/dashboard.js
+++ features/Dashboard/dashboard.js
@@ -10,17 +10,102 @@ const options = {
 };
 
 
+//fetch("../services/contributors.JSON")
+//.then(res => res.json())
+//.then(data => console.log(data))
+
+if (getEnvironment() === 3) { // MOCK
+
+  
+  uniLog("getInfo")
+  setTimeout(() => {  
+
+fetch("http://127.0.0.1:5501/core/service/models/mock/user.json")
+.then(res => res.json())
+.then(responseJson => {
+  // Access the JSON data and update HTML content
+  document.getElementById('nome').innerText = responseJson.name;
+  document.getElementById('email').innerText = responseJson.email;
+})
+
+   }, 2500);
+
+} else {
+
 fetch("http://localhost:3000/getInfo", options)
   .then(response => response.json())
   .then(responseJson => {
     // Access the JSON data and update HTML content
-    document.getElementById('nome').innerText = responseJson.name;
+    document.getElementById('name').innerText = responseJson.name;
     document.getElementById('email').innerText = responseJson.email;
   })
   .catch(error => console.error('Error fetching data:', error));
 
 
+  
 
+};
+
+if (getEnvironment() === 3) {
+  setTimeout(() => {  
+
+    fetch("http://127.0.0.1:5501/core/service/models/mock/getReservations.json")
+    .then(res => res.json())
+    .then(responseJson => {
+
+      const roomList = document.getElementById('roomList');
+  
+      // Clear existing content
+      roomList.innerHTML = '';
+  
+      // Loop through each object in the JSON data array
+      responseJson.forEach(item => {
+  
+        // Create list item element
+        const listItem = document.createElement('li');
+  
+        const date = new Date(item.dateTime);
+  
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+        const year = date.getFullYear();
+  
+        const formattedDate = `${day}-${month}-${year}`;
+  
+        // Populate list item with JSON data
+        listItem.innerHTML = `
+              <strong>Esame:</strong> <span>${item.examName}</span><br>
+              <strong>Corso di Studi:</strong> <span>${item.course}</span><br>
+              <strong>Sede:</strong> <span>${item.location}</span><br>
+              <strong>Giorno:</strong> <span>${formattedDate}</span>
+              <input type="button" id="roomListBtn" class="roomListBtn ${item.idExam}" value="Cancella prenotazione">
+              
+          `;
+  
+        // Append list item to the room list
+        roomList.appendChild(listItem);
+  
+        document.querySelectorAll(".roomListBtn").forEach(button => {
+          console.log(button.classList);
+  
+          if (button.classList[1] === `${item.idExam}`) {
+            button.addEventListener("click", function (event) {
+              event.preventDefault();
+              deleteReservation(item.idExam);
+              return;
+            });
+          }
+  
+        });
+  
+      });
+  
+    })
+  
+  
+  }, 2500);
+}
+else {
 // reservations
 fetch("http://localhost:3000/getReservations", options)
 
@@ -48,10 +133,11 @@ fetch("http://localhost:3000/getReservations", options)
 
       // Populate list item with JSON data
       listItem.innerHTML = `
-            <strong>Aula:</strong> <span>${item.roomType}</span><br>
-            <strong>Indirizzo sede:</strong> <span>${item.address}</span><br>
-            <strong>Giorno:</strong> <span>${formattedDate}</span>
-            <input type="button" id="roomListBtn" class="roomListBtn ${formattedDate}" value="Cancella prenotazione">
+             <strong>Esame:</strong> <span>${item.examName}</span><br>
+              <strong>Corso di Studi:</strong> <span>${item.course}</span><br>
+              <strong>Sede:</strong> <span>${item.location}</span><br>
+              <strong>Giorno:</strong> <span>${formattedDate}</span>
+              <input type="button" id="roomListBtn" class="roomListBtn ${item.idExam}" value="Cancella prenotazione">
             
         `;
 
@@ -61,10 +147,10 @@ fetch("http://localhost:3000/getReservations", options)
       document.querySelectorAll(".roomListBtn").forEach(button => {
         console.log(button.classList);
 
-        if (button.classList[1] === `${formattedDate}`) {
+        if (button.classList[1] === `${item.idExam}`) {
           button.addEventListener("click", function (event) {
             event.preventDefault();
-            deleteReservation(item.date);
+            deleteReservation(item.idExam);
             return;
           });
         }
@@ -76,7 +162,7 @@ fetch("http://localhost:3000/getReservations", options)
   })
   .catch(error => console.error('C`è stato un problema:', error));
 
-
+};
 
 // delete reservation call
 function deleteReservation(data) {
