ae9f7562f83ba3d73b413b034a8f8c5f0f7b718b
Giulia Floris ❀ <giulia.floris@accenture.com>
2025-02-05 11:43:44 +0100

    [SETUP] serverside

--- features/Supporto/supporto.js
+++
@@ -1,9 +0,0 @@
-
-
-
-fetch('http://localhost:2024/UniSignMeUp/v1/debugService', {
-    method: 'OPTIONS'
-})
-.catch(error => console.error('Error:', error));
-
-uniLog("debugService called", true)
